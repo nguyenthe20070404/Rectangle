438d63ee10ac779091546db160b94d5a778405e6
The Nguyen Van <kimbaobao1223@gmail.com>
2023-06-08 21:24:54 +0700

    complete

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub                                                                                                                              
+		Rectangle Hinhchunhat= new Rectangle(15,10); //tao mot doi tuong Student  
+		  System.out.println("chu vi la:"+Hinhchunhat.chuVi());  
+		  System.out.println("dien tich la:"+Hinhchunhat.dienTich()); 
+		  
+	}
+
+}
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,14 +1,20 @@
-import java.util.Set;
-
-/**
- * 
- */
-
-/**
- * @author asus
- *
- */
 public class Rectangle {
-	double chieuDai {Set}
+	double chieuDai;
+	private double chieuRong;
 
+	public Rectangle(double chieu_dai, double chieu_rong) {
+		chieuDai = chieu_dai;
+		chieuRong = chieu_rong;
+	}
+
+	public double chuVi() {
+		double chuVi;
+		chuVi = 2 * (chieuDai + chieuRong);
+		return chuVi;
+	}
+	public double dienTich() {
+		double dienTich;
+		dienTich =  (chieuDai * chieuRong);
+		return dienTich;
+	}
 }
